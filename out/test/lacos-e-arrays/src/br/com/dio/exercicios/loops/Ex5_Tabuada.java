acdf383afa3ca96f5fef77002298f7371b5bdbc7
Nilcelso Sobrinho <nilcelsop@gmail.com>
2021-11-05 18:58:31 -0300

    Atividade de fazer uma tabuada

--- out/test/lacos-e-arrays/src/br/com/dio/exercicios/loops/Ex5_Tabuada.java
+++ out/test/lacos-e-arrays/src/br/com/dio/exercicios/loops/Ex5_Tabuada.java
@@ -16,6 +16,18 @@ Tabuada de 5:
 */
 public class Ex5_Tabuada {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Tabuada: ");
+        int tabuada = scan.nextInt();
+
+        System.out.println("Tabuada de " + tabuada);
+
+        for (int i = 1; i < 11; i++) {
+            System.out.println(tabuada + " X " + i + " = " + (tabuada * i));
+        }
+
+
 
     }
 
--- src/br/com/dio/exercicios/loops/Ex5_Tabuada.java
+++ src/br/com/dio/exercicios/loops/Ex5_Tabuada.java
@@ -16,7 +16,16 @@ Tabuada de 5:
 */
 public class Ex5_Tabuada {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
 
+        System.out.println("Tabuada: ");
+        int tabuada = scan.nextInt();
+
+        System.out.println("Tabuada de " + tabuada);
+
+        for (int i = 1; i < 11; i++) {
+            System.out.println(tabuada + " X " + i + " = " + (tabuada * i));
+        }
     }
 
 }
