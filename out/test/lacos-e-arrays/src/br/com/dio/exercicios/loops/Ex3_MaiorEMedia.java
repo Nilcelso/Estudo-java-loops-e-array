35292f72b70f9e1dbb8a9a05380002be9dae9463
Nilcelso Sobrinho <nilcelsop@gmail.com>
2021-11-08 14:45:24 -0300

    Atividade para informar quantidade de numeros impares e pares foram inseridos pelo teclado

--- out/test/lacos-e-arrays/src/br/com/dio/exercicios/loops/Ex3_MaiorEMedia.java
+++ out/test/lacos-e-arrays/src/br/com/dio/exercicios/loops/Ex3_MaiorEMedia.java
@@ -25,7 +25,7 @@ public class Ex3_MaiorEMedia {
             if (numero > maior){
                 maior = numero;
             }
-            
+
             count++;
 
 
--- out/test/lacos-e-arrays/src/br/com/dio/exercicios/loops/Ex4_ParEImpar.java
+++ out/test/lacos-e-arrays/src/br/com/dio/exercicios/loops/Ex4_ParEImpar.java
@@ -9,6 +9,29 @@ e a quantidade de números impares.
 */
 public class Ex4_ParEImpar {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
+        int quantNumeros;
+        int numero;
+        int par = 0;
+        int impar = 0;
+        System.out.println("Quantidade de números: ");
+        quantNumeros = scan.nextInt();
+
+        int count = 0;
+        do{
+            System.out.println("Número: ");
+            numero = scan.nextInt();
+
+            if (numero % 2 ==0) par += 1;
+            else impar +=1;
+
+            count++;
+        }while(count < quantNumeros);
+
+        System.out.println(par + " Numero(s) par(es).");
+        System.out.println(impar + " Numero(s) impar(es)");
+
 
     }
 }
