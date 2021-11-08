4041eae3da0521ea5095e7d53b4be18dae740867
Nilcelso Sobrinho <nilcelsop@gmail.com>
2021-11-08 15:29:35 -0300

    Atividade de array em que mostra sua ordem inversa

--- out/test/lacos-e-arrays/src/br/com/dio/exercicios/arrays/Ex1_OrdemInversa.java
+++ out/test/lacos-e-arrays/src/br/com/dio/exercicios/arrays/Ex1_OrdemInversa.java
@@ -6,6 +6,17 @@ e mostre-os na ordem inversa.
 public class Ex1_OrdemInversa {
     public static void main(String[] args) {
 
+        int[] vetor = {0, -5, 15, 50, 8, 4};
+
+        int count = 0;
+        while (count < (vetor.length)){
+            System.out.println(vetor[count]);
+            count++;
+        }
+
+        for (int i = (vetor.length-1); i >= 0; i --) {
+            System.out.println(vetor[i] + " ");
+        }
 
     }
 }
